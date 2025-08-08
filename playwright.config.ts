3c9a0a1f94cce356f066ce377411857fb8ffe213
Alexander Drinkov <adrinkov@gmail.com>
2025-08-08 10:12:23 +0300

    Completed module8

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   reporter: 'html',
   fullyParallel: true,
+  workers: 2,
+  retries: 2,
+  maxFailures: 5, //If max number reached the test execution will stop
+
+  expect: {
+    timeout: 4000,
+  },
 
   use: {
     baseURL: 'http://localhost:3000/',
@@ -15,5 +22,43 @@ export default defineConfig({
   webServer: {
     command: 'npm start',
     url: 'http://localhost:3000/'
-  }
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+      testDir: './tests/mobile'
+    },
+    {
+      name: 'Smoke',
+      testMatch: /.*smoke.test.ts/,
+      retries: 0
+    },
+    {
+      name: 'Regular',
+      testIgnore: /.*regular.test.ts/,
+      retries: 2,
+      use: {
+
+      }
+    },
+    {
+      name: 'setup-cleanup-demo',
+      testMatch: '_4global_config.test.ts',
+      dependencies: ['setup']
+    },
+    {
+      name: 'setup',
+      testMatch: '**/global.setup.ts'
+    }
+  ]
 });
